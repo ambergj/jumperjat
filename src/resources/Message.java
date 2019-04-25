6d9703314d43bea6a9ff9aa759d74b63b76a0c94
Julian Amberg <julian.amberg@ict-campus.net>
2019-04-25 16:31:36 +0200

    completed Message Class

--- src/resources/Message.java
+++ src/resources/Message.java
@@ -1,4 +1,47 @@
 package resources;
 
+import java.time.LocalDateTime;
+
+/**
+ * A Class for the Message Object. It contains the Author, Timestamp and the Message Text.
+ *
+ * @author ambergj
+ * @version 2.0
+ * @since 1.8.0
+ */
 public class Message {
+    private String chatroomID;
+    private LocalDateTime timestamp;
+    private String author;
+    private String messageText;
+
+    /**
+     * Creates a Message Instance with these Params.
+     * @param chatroomID ID of the Chatroom to where the message belongs
+     * @param author who wrote the Message
+     * @param timestamp of the Message Date and Time ISO-8601
+     * @param messageText of Message
+     */
+    public Message(String chatroomID, String author, LocalDateTime timestamp, String messageText){
+        this.chatroomID = chatroomID;
+        this.timestamp = timestamp;
+        this.author = author;
+        this.messageText = messageText;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public String getChatroomID() {
+        return chatroomID;
+    }
 }
