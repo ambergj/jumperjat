0b0927430a95ec11cf5e0f4f189b4ea5c9fc93ab
Luescher Philippe <luescherphi@ict-campus.net>
2019-05-02 07:56:19 +0200

    Main-View des Clients ergänzt

--- src/clientView/MainViewController.java
+++ src/clientView/MainViewController.java
@@ -1,12 +1,16 @@
 package clientView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import client.Client;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
-public class MainViewController {
+public class MainViewController implements Initializable {
     
     private Client client;
 
@@ -18,6 +22,11 @@ public class MainViewController {
     
     @FXML
     private TabPane tpChatrooms;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
     
     public void setClient(Client client) {
         this.client = client;
