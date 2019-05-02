5e6c168fde40a20c400169bd82fa6c4012af4ded
Julian Amberg <julian.amberg@ict-campus.net>
2019-05-02 10:17:24 +0200

    Testing

---
+++ test/resources/ChatroomTest.java
@@ -0,0 +1,26 @@
+package resources;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChatroomTest {
+
+    @org.junit.jupiter.api.Test
+    void addUsers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addMessage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUserList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+}
---
+++ test/resources/UserTest.java
@@ -0,0 +1,22 @@
+package resources;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @org.junit.jupiter.api.Test
+    void equals1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUsername() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOutStream() {
+    }
+}
